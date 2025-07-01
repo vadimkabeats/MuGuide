efa63895e7d7af2323701dc07e42c7bce4e77807
Vadim-Li-V <vadker4@gmail.com>
2025-07-01 18:57:53 +0700

    добавил сущность Route

---
+++ app/src/main/java/com/example/myguide/data/entities/Route.kt
@@ -0,0 +1,12 @@
+package com.example.myguide.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "routes")
+data class Route(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val date: String
+)
