9c7440953c5bd7176a1e3ff0e3b1d4d322e79c2c
Vadim-Li-V <vadker4@gmail.com>
2025-07-01 18:54:31 +0700

    Добавил сущность Category

---
+++ app/src/main/java/com/example/myguide/data/entities/Category.kt
@@ -0,0 +1,11 @@
+package com.example.myguide.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "categories")
+data class Category(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String
+)
